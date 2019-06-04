6fff27f0a09c39065ab8565488cb894950b0b847
carlos-plaza <cplaza028@gmail.com>
2019-06-03 21:51:55 -0700

    added new grid button, and event listeners

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
 const container = document.querySelector('.container');
+const topMenu = document.querySelector('.top-menu');
 
-function createDivs() {
-  for (let i = 0; i < 16; i++) {
+const newGridBtn = document.createElement('button');
+newGridBtn.textContent = 'New Grid';
+topMenu.appendChild(newGridBtn);
+
+function generateRandomColor() {
+  let rgbValues = [];
+  for (let i = 0; i < 3; i++) {
+    let randomValue = Math.floor(Math.random() * 256);
+    rgbValues.push(randomValue);
+  }
+
+  let randomColor = `rgb(${rgbValues[0]},${rgbValues[1]},${rgbValues[2]})`;
+  return randomColor;
+}
+
+function createDivs(num) {
+
+  let gridSize = num * num;
+  console.log(gridSize);
+  for (let i = 0; i < gridSize; i++) {
     let newDiv = document.createElement('div');
-    newDiv.textContent = `I'm a div`;
     container.appendChild(newDiv);
   }
 }
 
-createDivs();
+function attachEventsToGridDivs() {
+  let gridDivs = document.querySelectorAll(".container > div");
+  gridDivs.forEach(element => {
+    element.addEventListener('mouseover', function() {
+      element.style.background = generateRandomColor();
+    });
+  });
+}
+
+function newGrid() {
+  // let gridDivs = document.querySelectorAll(".container > div");
+  // gridDivs.forEach(element => {
+  //   element.style.background = '#fff';
+  // });
+
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  let gridSize = parseInt( prompt('How many squares per side?','Min: 16, Max: 64') );
+  createDivs(gridSize);
+  attachEventsToGridDivs();
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+}
+
+window.addEventListener("load", createDivs(16));
+window.addEventListener("load", attachEventsToGridDivs);
+
+newGridBtn.addEventListener("click", newGrid);
